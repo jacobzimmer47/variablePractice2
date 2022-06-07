9f21524f4227fe38f927e7bf693309fd5732a4d6
Jacob Zimmer <jacobzimmer1@outlook.com>
2022-06-07 17:09:25 -0600

    Added new string

--- variable.js
+++ variable.js
@@ -7,3 +7,10 @@ var money = "0"
 var happy = "true"
 var sad = "Fasle"
 var excited = "True"
+var name = "megan"
+var town = "lonetree"
+var state = "AZ"
+console.log(state);
+console.log(town);
+console.log(money);
+console.log(age);
